bd701cb9557c95e14ed3facd1e9f831dc13a0d4e
LeandroCGMS <leandrocgms1986@outlook.com>
2018-04-27 10:41:46 -0400

    Exercícios.

---
+++ src/ControleFluxo4.java
@@ -0,0 +1,19 @@
+public class ControleFluxo4 {
+    public static void main(String[] args) {
+        int contador = 11;
+
+        while( contador < 10 ) {
+            System.out.println(contador);
+            contador++;
+        }
+
+        do {
+            System.out.println("dentro do do while");
+            contador++;
+        } while( contador < 10 );
+
+        for( int i = 0; i < 10; i++ ) {
+            System.out.println("o valor de i é: " + i);
+        }
+    }
+}
