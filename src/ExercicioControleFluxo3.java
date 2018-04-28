f0351d052a483dab8585060291b16e59a03a0f1d
LeandroCGMS <leandrocgms1986@outlook.com>
2018-04-28 19:23:24 -0400

    Exercícios.

--- src/ExercicioControleFluxo3.java
+++ src/ExercicioControleFluxo3.java
@@ -1,6 +1,6 @@
 public class ExercicioControleFluxo3 {
     public static void main(String[] args) {
-        //imprima todos os números pares de 0 até 100000
+        //imprima todos os números pares de 0 até 100000 ok
         for(int i = 0; i <= 100000; i += 2){
             System.out.println(i);
         }
