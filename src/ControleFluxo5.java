05368b0c64b68e9d222d38de5422f204b1b56ce9
LeandroCGMS <leandrocgms1986@outlook.com>
2018-04-28 20:03:44 -0400

    Exercícios.

---
+++ src/ControleFluxo5.java
@@ -0,0 +1,18 @@
+public class ControleFluxo5 {
+    public static void main(String[] args) {
+        //Dado um valor de um carro, descubra em quantas vezes ele pode ser parcelado
+        //Porém as parcelas não podem ser menores do que mil
+
+        double valorTotal = 30000;
+        for (int parcela = 1; parcela <= valorTotal; parcela++) {
+            double valorParcela = valorTotal / parcela;
+
+            if (valorParcela < 1000) {
+                break;
+            }
+
+            System.out.println("Parcela " + parcela + ": R$" + valorParcela);
+
+        }
+    }
+}
