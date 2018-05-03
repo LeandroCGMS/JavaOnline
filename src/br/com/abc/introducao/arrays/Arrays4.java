a851712a634747c57f7e3d3305434951e9656648
leandro <leandrocgms1986@outlook.com>
2018-05-02 21:03:33 -0400

    Criação de pacotes.

---
+++ src/br/com/abc/introducao/arrays/Arrays4.java
@@ -0,0 +1,10 @@
+package br.com.abc.introducao.arrays;
+
+public class Arrays4 {
+    public static void main(String[] args) {
+        String[] nomes = {"Juju", "Jaojao", "Xuxu"};
+        for (String nome : nomes) {
+            System.out.println(nome);
+        }
+    }
+}
