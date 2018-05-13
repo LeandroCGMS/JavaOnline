80999ad3b911aa72d98195b795fb02054dae7836
LeandroCGMS <leandrocgms1986@outlook.com>
2018-05-13 12:00:23 -0400

    Exercícios.

--- src/br/com/abc/javacore/sobrecargametodos/classes/Funcionario.java
+++ src/br/com/abc/javacore/sobrecargametodos/classes/Funcionario.java
@@ -1,21 +1,37 @@
 package br.com.abc.javacore.sobrecargametodos.classes;
 
+import java.util.Scanner;
+
 public class Funcionario {
     private String nome;
     private String cpf;
     private double salario;
     private String rg;
+    private Scanner scan = new Scanner(System.in);
 
-    public Funcionario(String nome, String cpf, double salario) {
+    public Funcionario(String nome, String cpf, double salario, String rg) {
+        //pequeno exemplo de validação no método construtor
+        if(nome.length() < 3){
+            System.out.println("Nome não pode ter menos que 3 caracteres. Tente novamente.");
+            new Funcionario(scan.nextLine(), cpf, salario, rg);
+        } else {
+            this.nome = nome;
+
+        }
+    }
+
+    public Funcionario() {
+
+    }
+
+    public void init(String nome, String cpf, double salario) {
         this.nome = nome;
         this.cpf = cpf;
         this.salario = salario;
     }
 
-    public Funcionario(String nome, String cpf, double salario, String rg) {
-        this.nome = nome;
-        this.cpf = cpf;
-        this.salario = salario;
+    public void init(String nome, String cpf, double salario, String rg) {
+        this.init(nome, cpf, salario);
         this.rg = rg;
     }
 
--- src/br/com/abc/javacore/sobrecargametodos/test/FuncionarioTest.java
+++ src/br/com/abc/javacore/sobrecargametodos/test/FuncionarioTest.java
@@ -4,9 +4,9 @@ import br.com.abc.javacore.sobrecargametodos.classes.Funcionario;
 
 public class FuncionarioTest {
     public static void main(String[] args) {
-        Funcionario funcionario = new Funcionario("Chica da Silva", "111.222.333-44", 4500,
+        Funcionario funcionario = new Funcionario();
+        funcionario.init("Chica da Silva", "111.222.333-44", 4500,
                 "122212-9");
-
         System.out.println(funcionario);
 
 
