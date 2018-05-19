261258edfd87c5ad59b29672e6dd85901113d719
LeandroCGMS <leandrocgms1986@outlook.com>
2018-05-19 12:10:49 -0400

    Herança.

---
+++ src/br/com/abc/javacore/Hheranca/test/HerancaTest.java
@@ -0,0 +1,25 @@
+package br.com.abc.javacore.Hheranca.test;
+
+import br.com.abc.javacore.Hheranca.classes.Endereco;
+import br.com.abc.javacore.Hheranca.classes.Funcionario;
+import br.com.abc.javacore.Hheranca.classes.Pessoa;
+
+public class HerancaTest {
+    public static void main(String[] args) {
+        Pessoa p = new Pessoa();
+        p.setNome("Gina");
+        p.setCpf("12345");
+        Endereco end = new Endereco();
+        end.setRua("Rua 25");
+        end.setBairro("Bairro 1");
+        p.setEndereco(end);
+        p.imprime();
+        System.out.println("-------------------------------------------");
+        Funcionario f = new Funcionario();
+        f.setNome("Augusto");
+        f.setCpf("99765");
+        f.setSalario(15000);
+        f.setEndereco(end);
+        f.imprime();
+    }
+}
