148b015b61a3c47793633ba1e61822614b060bd2
LeandroCGMS <leandrocgms1986@outlook.com>
2018-05-30 11:06:25 -0400

    Exceptions, parte 3.

---
+++ src/br/com/abc/javacore/Oexception/checkedexception/test/CheckedExceptionTest.java
@@ -0,0 +1,28 @@
+package br.com.abc.javacore.Oexception.checkedexception.test;
+
+import java.io.File;
+import java.io.IOException;
+
+public class CheckedExceptionTest {
+    public static void main(String[] args) {
+        try {
+            criarArquivo();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void criarArquivo() throws IOException {
+        File file = new File("Teste.txt");
+        try {
+            System.out.println("Erro ao criar o arquivo? " + file.createNewFile());
+            System.out.println("Arquivo criado com sucesso.");
+        } catch(IOException e){
+            e.printStackTrace();
+            throw e;
+        }
+
+
+
+    }
+}
--- src/br/com/abc/javacore/Oexception/runtimeexception/test/RuntimeExceptionTest.java
+++ src/br/com/abc/javacore/Oexception/runtimeexception/test/RuntimeExceptionTest.java
@@ -1,17 +1,30 @@
 package br.com.abc.javacore.Oexception.runtimeexception.test;
 
+import java.util.Scanner;
+
 public class RuntimeExceptionTest {
     public static void main(String[] args) {
-        Object o = null;
-//        int a = 10;
-//        int b = 0;
-//        if(b != 0) {
-//            int c = a / b;
-//            System.out.println(c);
-//        }
-        int[] a = new int[2];
-        System.out.println(a[2]);
+        Scanner scan = new Scanner(System.in);
+        int num1=0, num2=0;
+        System.out.println("Digite o número dividendo, seguido do número divisor pra obter o quociente.");
+        num1 = scan.nextInt();
+        num2 = scan.nextInt();
+        try{
+            divisao(num1, num2);
+        } catch(RuntimeException e){
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+            main(args);
+        }
 
 
+    }
+
+    private static void divisao(int num1, int num2){
+        if(num2 == 0){
+            throw new IllegalArgumentException("Passe um número diferente de 0 para o num2.");
+        }
+        int resultado = num1/num2;
+        System.out.println(resultado);
     }
 }
