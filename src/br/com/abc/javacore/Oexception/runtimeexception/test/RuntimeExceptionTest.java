4922a961cdf6fa4035a4ff67ade0142188d0824d
LeandroCGMS <leandrocgms1986@outlook.com>
2018-05-30 10:06:19 -0400

    Exceptions, parte 2.

---
+++ src/br/com/abc/javacore/Oexception/runtimeexception/test/RuntimeExceptionTest.java
@@ -0,0 +1,17 @@
+package br.com.abc.javacore.Oexception.runtimeexception.test;
+
+public class RuntimeExceptionTest {
+    public static void main(String[] args) {
+        Object o = null;
+//        int a = 10;
+//        int b = 0;
+//        if(b != 0) {
+//            int c = a / b;
+//            System.out.println(c);
+//        }
+        int[] a = new int[2];
+        System.out.println(a[2]);
+
+
+    }
+}
