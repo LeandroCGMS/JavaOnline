9a2e216b1819768802c549f54e9959e36e008714
LeandroCGMS <leandrocgms1986@outlook.com>
2018-06-03 11:03:25 -0400

    Try with resources.

--- src/br/com/abc/javacore/Oexception/runtimeexception/test/MultiplasRuntimeExceptionTest.java
+++ src/br/com/abc/javacore/Oexception/runtimeexception/test/MultiplasRuntimeExceptionTest.java
@@ -1,5 +1,6 @@
 package br.com.abc.javacore.Oexception.runtimeexception.test;
 
+import java.awt.*;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.SQLException;
@@ -10,27 +11,19 @@ public class MultiplasRuntimeExceptionTest {
             throw new IndexOutOfBoundsException();
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Dentro do ArrayIndexOutOfBoundException");
-        } catch (IndexOutOfBoundsException e) {
+        } catch (IllegalArgumentException | IndexOutOfBoundsException | ArithmeticException e) {
             System.out.println("Dentro do IndexOutOfBoundsException");
-        } catch (IllegalArgumentException e) {
-            System.out.println("Dentro do IllegalArgumentException");
-        } catch (ArithmeticException e) {
-            System.out.println("Dentro do ArithmeticException");
-        } catch (RuntimeException e) {
-            System.out.println("Dentro do RuntimeException");
         }
         System.out.println("Final do Programa");
 
         try {
             talvezLanceException();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private static void talvezLanceException() throws SQLException, FileNotFoundException {
+    private static void talvezLanceException() throws SQLException, AWTException, IOException {
 
     }
 }
