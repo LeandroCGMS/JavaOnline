23c049a1beb79132dc39a5e198502be4c45fd78f
LeandroCGMS <leandrocgms1986@outlook.com>
2018-06-03 12:17:14 -0400

    Sobrescrita com exception.

---
+++ src/br/com/abc/javacore/Oexception/checkedexception/classes/Funcionario.java
@@ -0,0 +1,11 @@
+package br.com.abc.javacore.Oexception.checkedexception.classes;
+
+import br.com.abc.javacore.Oexception.customexception.LoginInvalidoException;
+
+import java.io.FileNotFoundException;
+
+public class Funcionario extends Pessoa {
+    public void salvar() throws FileNotFoundException {
+
+    }
+}
---
+++ src/br/com/abc/javacore/Oexception/checkedexception/test/SobrescristaComExceptionsTest.java
@@ -0,0 +1,20 @@
+package br.com.abc.javacore.Oexception.checkedexception.test;
+
+import br.com.abc.javacore.Oexception.checkedexception.classes.Funcionario;
+import br.com.abc.javacore.Oexception.checkedexception.classes.Pessoa;
+import br.com.abc.javacore.Oexception.customexception.LoginInvalidoException;
+
+import java.io.FileNotFoundException;
+
+public class SobrescristaComExceptionsTest {
+    public static void main(String[] args) {
+        Funcionario f = new Funcionario();
+        Pessoa p = new Pessoa();
+        try {
+            f.salvar();
+        } catch (LoginInvalidoException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
